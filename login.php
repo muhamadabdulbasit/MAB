9fc71c99b97483dfa0f7483604e8822c96ce8032
Muhamad Abdul Basit <91742986+muhamadabdulbasit@users.noreply.github.com>
2021-11-17 11:57:26 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,4 @@
+<!DOCTYPE html>
+<input type="email" name="email" placeholder="enter email here">                
+                <input type="password" name="" placeholder="enter passswor here">
+                <li class="active"><button class="btnn" type="submit"><a href="login.html">Login</a></button></li>
